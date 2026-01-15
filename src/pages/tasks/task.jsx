7c3f0540c43ef37ddb07725df236f5e6171f0816
aliw-hash <aliw24136@gmail.com>
2026-01-15 23:37:29 +0530

    added style mx-auto to div

--- src/pages/tasks/task.jsx
+++ src/pages/tasks/task.jsx
@@ -1,3 +1,7 @@
 export default function Tasks() {
-  return <>Tasks Page</>;
+  return (
+    <section className="flex flex-row gap-8 justify-between h-min w-full p-4 bg-yellow-300">
+      <section></section>
+    </section>
+  );
 } 
